c7f6b7e6d51abb28bff80383c82e4e148036ba4a
dyh <yzcczdyh@gmail.com>
2018-05-05 23:26:34 +0800

    重构+evidence_1

--- analysis/test.py
+++ analysis/test.py
@@ -1,13 +1,91 @@
 import analysis.tool as tool
 import matplotlib.pyplot as plt
+import csv
+import numpy as np
 
-session_list = tool.get_leading_session('36400')
-y = tool.y
+tool.init()
 
-plt.figure()
-plt.plot(range(1, 731), y)
-plt.ylim(0, 300)
-plt.gca().invert_yaxis()
-plt.show()
+# y = tool.get_app_rank_daily('23350')
+#
+# plt.figure()
+# plt.plot(range(1, 731), y[1:])
+# plt.ylim(0, 300)
+# plt.gca().invert_yaxis()
+# plt.show()
+#
+# a = 1
+#
+# exit(0)
 
-a = 1
+# -------------------------------------------------------------计算所有app_id
+
+# ids = []
+# with open('../data/id_name_all.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     for line in reader:
+#         ids.append(line[0])
+#         ids = sorted(ids, key=int)
+#     tool.save_csv('data/ids', [ids])
+
+# -------------------------------------------------------------计算所有app排名
+
+# with open('../data/rank_list_all.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     rank_list_all_arr = list(reader)
+#
+# app_id_dic, ids = tool.get_app_id_dic_and_ids()
+# table = np.zeros((len(app_id_dic) + 1, 730 + 2), dtype=np.int32)
+# for row, id in enumerate(ids):
+#     table[row][0] = int(id)
+# for day, row in enumerate(rank_list_all_arr):
+#     for rank, item in enumerate(row):
+#         table[app_id_dic[item]][day + 1] = rank + 1
+#
+# tool.save_csv('data/app_rank_daily', table)
+
+# -------------------------------------计算所有session的sita, 存到./data/0.csv中
+
+# ids = tool.app_ids
+#
+# id_sitas = []
+# ii = 0
+# for id in ids:
+#     sessions = tool.get_leading_session(id)
+#     sitas = [id]
+#     for session in sessions:
+#         sita = session.get_sita()
+#         if sita > 0:
+#             sitas.append(sita)
+#     id_sitas.append(sitas)
+#     print(id)
+#
+#     # if len(id_sitas) == 100:
+#     #     tool.save_csv('data/' + str(ii), id_sitas)
+#     #     ii += 1
+#     #     id_sitas = []
+#
+# tool.save_csv('data/' + str(ii), id_sitas)
+#
+# a = 1
+
+
+# -------------------------------------计算所有session的sita
+
+# ids = tool.app_ids
+#
+# sitas = []
+# for id in ids:
+#     sessions = tool.get_leading_session(id)
+#     for session in sessions:
+#         sita = session.get_sita()
+#         if sita > 0:
+#             sitas.append(sita)
+#
+# sitas_mean = np.mean(sitas)  # 3.061398376330555
+# sitas_var = np.var(sitas)  # 0.026046925110970412
+#
+# a = 1
+
+# -------------------------------------计算所有get_evidence_1
+
+tool.get_evidence_1('23796')
--- analysis/tool.py
+++ analysis/tool.py
@@ -1,11 +1,21 @@
 import csv
 import math
 
-rank_list_all_arr = None
+# 计算得出
+sitas_mean = 3.061398376330555
+sitas_var = 0.026046925110970412
+
 K = 300
 R = 100
 T = 7
-y = []
+
+rank_list_all_arr = None
+app_index_dic = None
+app_ids = None
+
+
+def init():
+    get_app_id_dic_and_ids()
 
 
 class LeadingEvent:
@@ -13,22 +23,40 @@ class LeadingEvent:
     endTime = 0
     mid1Time = None
     mid2Time = None
+    degreeLeft = None
+    degreeRight = None
+
+    def get_degree(self):
+        if self.degreeLeft and self.degreeRight:
+            return self.degreeLeft + self.degreeRight
+        return 0
 
 
 class LeadingSession:
     eventList = None
     startTime = None
     endTime = None
+    degreeArr = None
 
     def add_event(self, leading_event):
         if not self.eventList:
             self.eventList = []
         self.eventList.append(leading_event)
         self.endTime = leading_event.endTime
+        if not self.degreeArr:
+            self.degreeArr = []
+        event_degree = leading_event.get_degree()
+        if event_degree:
+            self.degreeArr.append(leading_event.get_degree())
+
+    def get_sita(self):
+        if not self.degreeArr or len(self.degreeArr) == 0:
+            return 0
+        return sum(self.degreeArr) / len(self.degreeArr)
 
 
 def get_leading_session(app_id):
-    global rank_list_all_arr, K, R, T
+    global T
 
     event_list = get_leading_event(app_id)
 
@@ -54,29 +82,23 @@ def get_leading_session(app_id):
 
 
 def get_leading_event(app_id):
-    global rank_list_all_arr, K, R, T, y
+    global K, R
 
-    if not rank_list_all_arr:
-        with open('../data/rank_list_all.csv', 'r') as f:
-            reader = csv.reader(f)
-            rank_list_all_arr = list(reader)
-
-    for line in rank_list_all_arr:
-        try:
-            y.append(line.index(app_id) + 1)
-        except ValueError:
-            y.append(10000)
+    y = get_app_rank_daily(app_id)
 
     event_list = []
     event = None
-    for i in range(1, len(y) + 1):
-        if y[i - 1] <= K:
+    for i in range(1, len(y)):
+        if y[i] <= K:
             if event:
-                event.endTime = i
-                if y[i - 1] <= R:
-                    event.mid2Time = i
+                if y[i] <= R:
                     if not event.mid1Time:
                         event.mid1Time = i
+                        event.degreeLeft = math.atan((K - y[i]) / (i - event.startTime))
+                    event.mid2Time = i
+                event.endTime = i
+                if event.mid2Time and i > event.mid2Time:
+                    event.degreeRight = math.atan((K - y[event.mid2Time]) / (i - event.mid2Time))
             else:
                 event = LeadingEvent()
                 event.startTime = event.endTime = i
@@ -89,6 +111,22 @@ def get_leading_event(app_id):
     return event_list
 
 
+def get_app_rank_daily(app_id):
+    global rank_list_all_arr, app_index_dic
+
+    if not rank_list_all_arr:
+        with open('data/app_rank_daily.csv', 'r') as f:
+            reader = csv.reader(f)
+            rank_list_all_arr = list(reader)
+
+    app_index = app_index_dic[app_id]
+    res = list(map(int, rank_list_all_arr[app_index][0:731]))
+    for index, item in enumerate(res):
+        if not item:
+            res[index] = 10000
+    return res
+
+
 def save_csv(file_name, data):
     with open(file_name + ".csv", "w", newline="", encoding='utf-8') as w:
         writer = csv.writer(w)
@@ -96,41 +134,70 @@ def save_csv(file_name, data):
             writer.writerow(row)
 
 
-def get_session_seita_s(session):
-    degree_sum = []
-    for event in session.eventList:
+# def get_session_sita_s(session):
+#     degree_sum = []
+#     for event in session.eventList:
+#
+#         start_time = event.startTime
+#         end_time = event.endTime
+#         middle1_time = event.mid1Time
+#         middle2_time = event.mid2Time
+#         if not middle1_time or not middle2_time == 0:
+#             continue
+#
+#         if middle1_time - start_time > 0 and end_time - middle2_time > 0:
+#             degree1 = math.atan((K - y[middle1_time - 1]) / (middle1_time - start_time))
+#             degree2 = math.atan((K - y[middle2_time - 1]) / (end_time - middle2_time))
+#             degree_sum.append(degree1 + degree2)
+#
+#     if len(degree_sum) == 0:
+#         return 0
+#
+#     return sum(degree_sum) / len(degree_sum)
 
-        start_time = event.startTime
-        end_time = event.endTime
-        middle1_time = event.mid1Time
-        middle2_time = event.mid2Time
-        if middle1_time == 0 or middle2_time == 0:
-            continue
 
-        if middle1_time - start_time > 0 and end_time - middle2_time > 0:
-            degree1 = math.atan((K - y[middle1_time - 1]) / (middle1_time - start_time))
-            degree2 = math.atan((K - y[middle2_time - 1]) / (end_time - middle2_time))
-            degree_sum.append(degree1 + degree2)
+def get_app_id_dic_and_ids():
+    global app_index_dic, app_ids
+    if app_index_dic and app_ids:
+        return app_index_dic, app_ids
 
-    if len(degree_sum) == 0:
-        return 0
+    ids = []
+    with open('data/ids.csv', 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            ids = line
+    dic = {}
+    for index, id in enumerate(ids):
+        dic[id] = index
 
-    return sum(degree_sum) / len(degree_sum)
+    app_index_dic = dic
+    app_ids = ids
+    return dic, ids
+
+
+def get_session_p(session: LeadingSession):
+    global sitas_mean, sitas_var
+
+    return 1 / 2 * (1 + math.erf((session.get_sita() - sitas_mean) / (sitas_var * math.sqrt(2))))
 
 
 def get_evidence_1(app_id):
-    # global rank_list_all_arr, K, R, T, y
-    #
-    # session_list = get_leading_session(app_id)
-    #
-    # degrees = []
-    #
-    # for session in session_list:
-    #     degree_avg = get_session_seita_s(session)
-    #     degrees.append(degree_avg)
-    #
-    # # 计算全局的 n1, n2
-    # n1 = 1  # 平均值
-    # n2 = 1  # 方差
-    #
-    # return 1 / 2 * (1 + math.erf((sdegree - n1) / (n2 * math.sqrt(2))))
+    sessions = get_leading_session(app_id)
+    for session in sessions:
+        print(get_session_p(session))
+
+# global rank_list_all_arr, K, R, T, y
+#
+# session_list = get_leading_session(app_id)
+#
+# degrees = []
+#
+# for session in session_list:
+#     degree_avg = get_session_seita_s(session)
+#     degrees.append(degree_avg)
+#
+# # 计算全局的 n1, n2
+# n1 = 1  # 平均值
+# n2 = 1  # 方差
+#
+# return 1 / 2 * (1 + math.erf((sdegree - n1) / (n2 * math.sqrt(2))))
